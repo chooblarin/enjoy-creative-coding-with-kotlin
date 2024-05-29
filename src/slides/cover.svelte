7732b8f1ab7eef19f1903e3f070bea32aa4f1b4a
Sota Hatakeyama <choo.bla.rin@gmail.com>
2024-05-30 08:57:19 +0900

    Install Noto Serif JP font

--- src/slides/cover.svelte
+++ src/slides/cover.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <Slide>
-  <div class="font-bold">Kotlinで<ruby>愉<rp>(</rp><rt>たの</rt><rp>)</rp></ruby>しむクリエイティブコーディング</div>
+  <div class="text-5xl font-bold">Kotlinで<ruby>愉<rp>(</rp><rt>たの</rt><rp>)</rp></ruby>しむクリエイティブコーディング</div>
   <p class="text-3xl">2024.06.22</p>
   <p class="text-3xl">畠山 創太</p>
 </Slide>
