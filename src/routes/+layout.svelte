a78c2c92508b54764e37a14adc3487f0bcfb12a8
Sota Hatakeyama <choo.bla.rin@gmail.com>
2024-05-30 22:56:44 +0900

    Preload cretical font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,16 @@
 <script>
+  import openSansWoff2 from '@fontsource-variable/noto-serif-jp/files/noto-serif-jp-latin-wght-normal.woff2?url';
+
   import 'reveal.js/dist/reveal.css';
   import 'reveal.js/dist/theme/white.css';
 
   import '$assets/styles/app.css';
   import '$assets/styles/reveal-custom.css';
 </script>
+
+<svelte:head>
+  <link rel="preload" as="font" type="font/woff2" href={openSansWoff2} />
+</svelte:head>
 
 <slot></slot>
 
