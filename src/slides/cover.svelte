760416feb9e383c2da30b6bccb34e5389ef25b2d
Sota Hatakeyama <choo.bla.rin@gmail.com>
2024-05-30 09:07:15 +0900

    Add kotlin fest logo

--- src/slides/cover.svelte
+++ src/slides/cover.svelte
@@ -1,9 +1,11 @@
 <script>
 	import Slide from "$lib/components/slide.svelte";
+	import logo from "../assets/images/kotlin-fest-2024/Logo_Landscape_Mono.svg";
 </script>
 
 <Slide>
   <div class="text-5xl font-bold">Kotlinで<ruby>愉<rp>(</rp><rt>たの</rt><rp>)</rp></ruby>しむクリエイティブコーディング</div>
   <p class="text-3xl">2024.06.22</p>
   <p class="text-3xl">畠山 創太</p>
+  <img src={logo} alt="Kotlin Fest 2024" />
 </Slide>
