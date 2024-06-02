8bb5983cc1cb4de07ffed30920890e1cf511a03b
Sota Hatakeyama <choo.bla.rin@gmail.com>
2024-06-02 11:14:31 +0900

    Rive demo

--- src/presentation.svelte
+++ src/presentation.svelte
@@ -4,6 +4,7 @@
 
   import Cover from './slides/cover.svelte';
   import KotlinXCreativeCoding from './slides/kotlin-x-creative-coding.svelte';
+  import RiveDemo from './slides/rive-demo.svelte';
   import TeachCodingToStudents from './slides/teach-coding-to-students.svelte';
   import WhatIsCreativeCoding from './slides/what-is-creative-coding.svelte';
   import WhatIsOpenrndr from './slides/what-is-openrndr.svelte';
@@ -41,3 +42,5 @@
   }
   `}</Code>
 </Slide>
+
+<RiveDemo />
---
+++ src/slides/rive-demo.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import Slide from "$lib/components/slide.svelte";
+  import { Rive } from "@rive-app/canvas";
+  import { onMount } from "svelte";
+
+  let canvas: HTMLCanvasElement;
+
+  onMount(() => {
+    const r = new Rive({
+        src: "https://cdn.rive.app/animations/vehicles.riv",
+        canvas,
+        autoplay: true,
+        stateMachines: "bumpy",
+        onLoad: () => {
+          r.resizeDrawingSurfaceToCanvas();
+        },
+    });
+    return () => r.cleanup();
+  });
+
+</script>
+
+<Slide>
+  <canvas bind:this={canvas} width="500" height="500" class="w-full h-auto max-w-[640px] max-h-[640px] mx-auto"></canvas>
+</Slide>
