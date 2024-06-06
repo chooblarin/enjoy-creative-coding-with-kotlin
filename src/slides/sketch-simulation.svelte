579b1fa916c147103801e1a2618667776ee78dce
Sota Hatakeyama <choo.bla.rin@gmail.com>
2024-06-07 05:37:42 +0900

    Prepare for simulation

--- src/slides/sketch-simulation.svelte
+++ src/slides/sketch-simulation.svelte
@@ -6,3 +6,18 @@
   <div class="text-8xl mb-4">Simulation</div>
   <div>シミュレーション</div>
 </Slide>
+
+<Slide>
+  <div>DLA</div>
+  <!-- TODO -->
+</Slide>
+
+<Slide>
+  <div>Boid</div>
+  <!-- TODO -->
+</Slide>
+
+<Slide>
+  <div>Physarum</div>
+  <!-- TODO -->
+</Slide>
