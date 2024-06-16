5a9b817453fc7ac1ec36cc7aca36fd80f29084f8
Sota Hatakeyama <choo.bla.rin@gmail.com>
2024-06-16 13:48:00 +0900

    Tweak cover

--- src/slides/cover.svelte
+++ src/slides/cover.svelte
@@ -6,13 +6,11 @@
 </script>
 
 <Slide backgroundColor="black" backgroundIframe={background}>
-  <div class="font-semibold r-fit-text text-start">
+  <div class="r-fit-text text-start font-semibold [text-shadow:_0_2px_0_rgb(0_0_0_/_90%)]">
     <div>
       Kotlinで<ruby>愉<rp>(</rp><rt>たの</rt><rp>)</rp></ruby>しむ
     </div>
-    <div>
-      クリエイティブコーディング
-    </div>
+    <div>クリエイティブコーディング</div>
   </div>
-  <div class="text-3xl mt-20">畠山 創太</div>
+  <div class="mt-20 text-3xl">畠山 創太</div>
 </Slide>
